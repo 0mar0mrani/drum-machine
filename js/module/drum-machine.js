014773564651fb8c690922089a7697a679dacf1d
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-11-15 22:18:21 +0100

    Made drum machine able to create drum pattern by user

--- js/module/drum-machine.js
+++ js/module/drum-machine.js
@@ -2,60 +2,59 @@ export default function DrumMachine() {
 	// Data
 	const bpm = 120;
 	const bpmInMilliseconds = (60 / bpm) * 1000;
+	const sixteenthNoteInMilliseconds = bpmInMilliseconds / 4
 
-
-	const halfNote = bpmInMilliseconds * 2;
-	const eightNote = bpmInMilliseconds / 2
-	
+	const pattern = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,]
+	let currentPatternIndex = 0
 	let isPlaying = false; 
+	let sampleInterval;
+
+
+	// QuerrySelectors
 	const playButton = document.querySelector('.drum-machine__play-button');
+	const checkBoxes = document.querySelectorAll('.drum-machine__checkbox')
 
-	const kick = new Audio('/assets/audio/kick.wav'); 
-	const snare = new Audio('/assets/audio/snare.wav'); 
-	const hihatClosed = new Audio('/assets/audio/hihat-closed.wav'); 
-
+	// Eventlisteners
 	playButton.addEventListener('click', handlePlayButtonClick)
 
+	for (let index = 0; index < checkBoxes.length; index += 1) {
+		checkBoxes[index].addEventListener('change', () => {
+			handleCheckboxChange(event, index);
+		})
+	}
+
+	// Methods
+	function handleCheckboxChange(event, index) {
+		pattern[index] = !pattern[index];
+		console.log(pattern);
+	}
+	
 	function handlePlayButtonClick() {
 		isPlaying = !isPlaying;
 
 		toggleKickPattern();
-		toggleSnarePattern();
-		toggleHihatClosedPattern();
-	}
-
-	let kickPattern;
-	let hatPattern;
-	let snarePattern;
-
-	function toggleHihatClosedPattern() {
-		if (isPlaying) {
-			hatPattern = setInterval(function() {
-				hihatClosed.play();
-			}, eightNote) 
-		} else {
-			clearInterval(hatPattern)
-		}
 	}
 
 	function toggleKickPattern() {
-
 		if (isPlaying) {
-			kickPattern = setInterval(function() {
-				kick.play();
-			}, bpmInMilliseconds) 
-		} else {
-			clearInterval(kickPattern)
-		}
-	}
+			sampleInterval = setInterval(function() {
+				if (pattern[currentPatternIndex]) {
+					const kick = new Audio('/assets/audio/kick.wav'); 
+					kick.play()
+				} 
+				
+				console.log(`Current index is ${currentPatternIndex}`);
 
-	function toggleSnarePattern() {
-		if (isPlaying) {
-			snarePattern = setInterval(function() {
-				snare.play();
-			}, halfNote) 
+				if (currentPatternIndex === 15) {
+					currentPatternIndex = 0;
+				} else {
+					currentPatternIndex += 1;
+				}
+
+			}, sixteenthNoteInMilliseconds) 
+
 		} else {
-			clearInterval(snarePattern)
+			clearInterval(sampleInterval)
 		}
 	}
 }
